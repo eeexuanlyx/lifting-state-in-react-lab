3676af93bed8f522d40f5e2408ff472f99d4fe2c
eeexuanlyx <eeexuanlyx@gmail.com>
2024-10-16 17:47:06 +0800

    Add Conditional Rendering

--- src/component/BurgerStack.jsx
+++ src/component/BurgerStack.jsx
@@ -2,12 +2,16 @@
 const BurgerStack = (props) => {
   return (
     <ul>
-      {props.ingredients.map((ingredient, idx) => (
-        <li style={{ backgroundColor: ingredient.color }} key={idx}>
-          {ingredient.name}
-          <button onClick={() => props.removeItem(ingredient)}>-</button>
-        </li>
-      ))}
+      {props.ingredients.length !== 0 ? (
+        props.ingredients.map((ingredient, idx) => (
+          <li style={{ backgroundColor: ingredient.color }} key={idx}>
+            {ingredient.name}
+            <button onClick={() => props.removeItem(ingredient)}>-</button>
+          </li>
+        ))
+      ) : (
+        <p>no ingredient</p>
+      )}
     </ul>
   );
 };
